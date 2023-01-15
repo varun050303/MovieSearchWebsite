febea6aad09009951632aa291d165ed437d4b7f3
Varun singhal <78909090+varun050303@users.noreply.github.com>
2023-01-15 23:20:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TMDB 
 
-const API_KEY = 'api_key=b09a819349db8dad3aa1f239ed43c9f1';
+const API_KEY = 'use your api key here';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
